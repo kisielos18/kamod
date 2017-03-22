f5e7cef6a412c806ed4eaad52be4cb4441464388
kisielos18 <j.kisiel4@gmail.com>
2017-03-23 00:04:49 +0100

    Added test modules

---
+++ inc/test.h
@@ -0,0 +1,13 @@
+/*
+ * test.h
+ *
+ *  Created on: 23 mar 2017
+ *      Author: kuba
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+#define error "To jest error dodany poprzez mojego commita huehueheuheu ;)"
+
+#endif /* TEST_H_ */
